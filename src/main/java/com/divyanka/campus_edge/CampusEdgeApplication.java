a0f2042e1dd67b0b19b0bd18cc813682c976715c
dell <pagaredivyanka@gmail.com>
2026-06-25 20:57:21 +0530

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/divyanka/campus_edge/CampusEdgeApplication.java
@@ -0,0 +1,13 @@
+package com.divyanka.campus_edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusEdgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divyanka/campus_edge/CampusEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyanka.campus_edge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
